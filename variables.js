ef5f96f9cefba52242de5c409e7c9b48f3a5d3b0
Tshegofatso Mailula <tshego.mailula@gmail.com>
2026-05-26 19:57:54 +0200

    Initialize javascript repo

---
+++ variables.js
@@ -0,0 +1,27 @@
+//Exercise 1
+
+let name = "Tshegofatso Mailula";
+let age = "31";
+let isStudent = false;
+console.log(name);
+console.log(age);
+console.log(false);
+
+//Exercise 2
+
+let favoriteColour = "blue";
+console.log(`\nMy favorite colour is ${favoriteColour}`);
+favoriteColour = "red";
+console.log(`My new favourite colour is ${favoriteColour}\n`);
+
+//Exercise 3
+let num1 = 24;
+let num2 = 25;
+let sum = num1 + num2;
+let difference = num2 - num1;
+let product = num1 * num2;
+let quotient = num2 % num1;
+console.log(`Sum of ${num1} + ${num2} is ${sum}`);
+console.log(`Difference of ${num2} - ${num1} is ${difference}`);
+console.log(`Product of ${num1} * ${num2} is ${product}`);
+console.log(`Quotient of ${num2} % ${num1} is ${quotient}`);
