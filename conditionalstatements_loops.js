4216082c486eb584d54e2e2599fa0da21f612750
Tshegofatso Mailula <tshego.mailula@gmail.com>
2026-05-26 21:04:21 +0200

    Update while loops

--- conditionalstatements_loops.js
+++ conditionalstatements_loops.js
@@ -16,6 +16,24 @@ else if(temperature>25)
 {
     console.log("It's warm");
 }
+switch(true)
+{
+    case temperature<0:
+        console.log("It's freezing!");
+        break;
+    case temperature>=0 && temperature<=15:
+        console.log("It's cold");
+        break;
+    case temperature>=16 && temperature <=25:
+        console.log("It's mild");
+        break;
+    case temperature>25:
+        console.log("It's warm");
+        break;
+    default:
+        console.log("Invalid temperature")
+}
+
 //Exercise 2: Divisbility Check
 let number = 67;
 if(number%2==0 && number%3==0)
@@ -32,6 +50,21 @@ else
 {
     console.log("Not divisible by 2 or 3");
 }
+switch(true)
+{
+    case number%2==0 && number%3==0:
+        console.log("Divisible by both");
+        break;
+    case number%2==0:
+        console.log("Divisible only by 2");
+        break;
+    case number%3==0:
+        console.log("Divisible only by 3");
+        break;
+    default:
+        console.log("Not divisible by 2 or 3");
+}
+
 //Exercise 3: For loops
 //1
 console.log("\n");
@@ -75,8 +108,8 @@ for(i=0;i<numbers_set.length;i++)
     }
 }
 console.log(`\nThe largest number is ${largest_number}`);
-//Exercise 4: While loops
 
+//Exercise 4: While loops
 //1
 console.log(`\n`);
 let while_loop = 1;
@@ -97,7 +130,16 @@ while(while_loop<=20)
     }
     while_loop++;
 }
-
+//3
+console.log('\n');
+let while_loop_total = 0;
+let new_while_loop = 1;
+while(new_while_loop<=100)
+{
+    while_loop_total = while_loop_total + new_while_loop;
+    new_while_loop++
+}
+console.log(`While loop total ${while_loop_total}`);   
 //4
 console.log('\n')
 while_loop = 1;
@@ -109,6 +151,7 @@ while(while_loop<=50)
     }
     while_loop++;
 }
+
 //Exercise 5: Do while loops
 //1
 console.log("\n");
